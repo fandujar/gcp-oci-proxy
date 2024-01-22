66f9ff541601a52e6426f45f21b352c0f4c2017b
Filipe Andujar <fandujar@totvs.ai>
2024-01-22 20:56:36 -0300

    feat: first version

--- go.mod
+++ go.mod
@@ -3,33 +3,77 @@ module totvs.ai/gcp-oci-proxy
 go 1.20
 
 require (
-	cloud.google.com/go v0.110.10 // indirect
-	cloud.google.com/go/artifactregistry v1.14.6 // indirect
+	cloud.google.com/go/artifactregistry v1.14.6
+	github.com/go-chi/chi v1.5.5
+	google.golang.org/api v0.157.0
+)
+
+require (
+	cloud.google.com/go v0.111.0 // indirect
 	cloud.google.com/go/compute v1.23.3 // indirect
 	cloud.google.com/go/compute/metadata v0.2.3 // indirect
 	cloud.google.com/go/iam v1.1.5 // indirect
 	cloud.google.com/go/longrunning v0.5.4 // indirect
-	github.com/go-chi/chi v1.5.5 // indirect
+	github.com/AdaLogics/go-fuzz-headers v0.0.0-20230811130428-ced1acdcaa24 // indirect
+	github.com/Azure/go-ansiterm v0.0.0-20210617225240-d185dfc1b5a1 // indirect
+	github.com/Masterminds/semver/v3 v3.2.1 // indirect
+	github.com/Microsoft/hcsshim v0.11.4 // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/containerd/containerd v1.7.11 // indirect
+	github.com/containerd/log v0.1.0 // indirect
+	github.com/docker/cli v24.0.6+incompatible // indirect
+	github.com/docker/distribution v2.8.2+incompatible // indirect
+	github.com/docker/docker v24.0.7+incompatible // indirect
+	github.com/docker/docker-credential-helpers v0.7.0 // indirect
+	github.com/docker/go-connections v0.4.0 // indirect
+	github.com/docker/go-metrics v0.0.1 // indirect
+	github.com/docker/go-units v0.5.0 // indirect
+	github.com/felixge/httpsnoop v1.0.4 // indirect
+	github.com/go-logr/logr v1.3.0 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/google/s2a-go v0.1.7 // indirect
 	github.com/googleapis/enterprise-certificate-proxy v0.3.2 // indirect
 	github.com/googleapis/gax-go/v2 v2.12.0 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/klauspost/compress v1.16.0 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
+	github.com/moby/locker v1.0.1 // indirect
+	github.com/moby/term v0.5.0 // indirect
+	github.com/morikuni/aec v1.0.0 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.1.0-rc5 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/prometheus/client_golang v1.16.0 // indirect
+	github.com/prometheus/client_model v0.4.0 // indirect
+	github.com/prometheus/common v0.44.0 // indirect
+	github.com/prometheus/procfs v0.10.1 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
 	go.opencensus.io v0.24.0 // indirect
-	gocloud.dev v0.36.0 // indirect
-	golang.org/x/crypto v0.17.0 // indirect
-	golang.org/x/net v0.18.0 // indirect
-	golang.org/x/oauth2 v0.14.0 // indirect
-	golang.org/x/sync v0.5.0 // indirect
-	golang.org/x/sys v0.15.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc v0.46.1 // indirect
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.46.1 // indirect
+	go.opentelemetry.io/otel v1.21.0 // indirect
+	go.opentelemetry.io/otel/metric v1.21.0 // indirect
+	go.opentelemetry.io/otel/trace v1.21.0 // indirect
+	golang.org/x/crypto v0.18.0 // indirect
+	golang.org/x/net v0.20.0 // indirect
+	golang.org/x/oauth2 v0.16.0 // indirect
+	golang.org/x/sync v0.6.0 // indirect
+	golang.org/x/sys v0.16.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
-	golang.org/x/time v0.4.0 // indirect
-	golang.org/x/xerrors v0.0.0-20231012003039-104605ab7028 // indirect
-	google.golang.org/api v0.151.0 // indirect
+	golang.org/x/time v0.5.0 // indirect
 	google.golang.org/appengine v1.6.8 // indirect
-	google.golang.org/genproto v0.0.0-20231120223509-83a465c0220f // indirect
-	google.golang.org/genproto/googleapis/api v0.0.0-20231120223509-83a465c0220f // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20231120223509-83a465c0220f // indirect
-	google.golang.org/grpc v1.59.0 // indirect
-	google.golang.org/protobuf v1.31.0 // indirect
+	google.golang.org/genproto v0.0.0-20240102182953-50ed04b92917 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20240102182953-50ed04b92917 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240116215550-a9fa1716bcac // indirect
+	google.golang.org/grpc v1.60.1 // indirect
+	google.golang.org/protobuf v1.32.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	helm.sh/helm/v3 v3.14.0 // indirect
+	k8s.io/client-go v0.29.0 // indirect
+	oras.land/oras-go v1.2.4 // indirect
+	sigs.k8s.io/yaml v1.3.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
+	"bytes"
 	"context"
-	"encoding/json"
 	"fmt"
 	"io"
 	"log"
@@ -16,8 +16,36 @@ import (
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 
+	"helm.sh/helm/v3/pkg/registry"
+
 	artifactregistry "cloud.google.com/go/artifactregistry/apiv1"
 	artifactregistrypb "cloud.google.com/go/artifactregistry/apiv1/artifactregistrypb"
+	"google.golang.org/api/iterator"
+)
+
+type Config struct {
+	Project    string
+	Repository string
+	Region     string
+	Port       string
+	Credential string
+}
+
+type Repository struct {
+	Assets []*Asset `json:"assets"`
+}
+
+type Asset struct {
+	Name      string    `json:"name"`
+	SHA       string    `json:"sha"`
+	RawName   string    `json:"raw_name"`
+	URI       string    `json:"uri"`
+	MediaType string    `json:"media_type"`
+	Tags      []*string `json:"tags"`
+}
+
+var (
+	RepositoryDB *Repository = &Repository{}
 )
 
 func newServer(router *chi.Mux) *http.Server {
@@ -47,113 +75,22 @@ func defaultHealthCheck(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "ok")
 }
 
-func isRequestAuthorized(input string) bool {
-	auths, err := getAuths()
-	if err != nil {
-		log.Println(err)
-		return false
-	}
-
-	for _, auth := range auths {
-		if input == auth {
-			return true
-		}
-	}
-	return false
+func formatPath(config *Config) (string, error) {
+	return fmt.Sprintf(
+		"projects/%s/locations/%s/repositories/%s",
+		config.Project, config.Region, config.Repository,
+	), nil
 }
 
-func getAuths() ([]string, error) {
-	auths := os.Getenv("AUTHS")
-	if auths == "" {
-		return nil, fmt.Errorf("missing auths")
-	}
-	return strings.Split(auths, ","), nil
-}
-
-func getAssetsHandler(ctx context.Context) func(w http.ResponseWriter, r *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
-		var path = chi.URLParam(r, "asset")
-		if strings.Contains(path, ".ico") {
-			w.WriteHeader(http.StatusNotFound)
-			w.Write([]byte("404 Not Found\n"))
-			return
-		}
-
-		// user, pwd, ok := r.BasicAuth()
-		// if !ok {
-		// 	log.Println("missing basic auth")
-		// 	w.WriteHeader(http.StatusUnauthorized)
-		// 	w.Write([]byte("401 Unauthorized\n"))
-		// 	return
-		// }
-
-		// if !(ok && isRequestAuthorized(user+":"+pwd)) {
-		// 	log.Println("unauthorized:", user)
-		// 	w.WriteHeader(http.StatusUnauthorized)
-		// 	w.Write([]byte("401 Unauthorized\n"))
-		// }
-
-		reader, err := getImageMetadataReader(ctx, path)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte(err.Error()))
-			return
-		}
-
-		if _, err := io.Copy(w, reader); err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte(err.Error()))
-			return
-		}
-	}
-}
-
-func getImageMetadataReader(ctx context.Context, path string) (io.Reader, error) {
-	if path == "" {
-		return nil, fmt.Errorf("missing path")
-	}
-	if !strings.HasPrefix(path, "/") {
-		path = "/" + path
-	}
-
-	c, err := artifactregistry.NewClient(ctx)
-	if err != nil {
-		return nil, err
-	}
-	defer c.Close()
-
-	formattedPath, err := formatPath(path)
-	if err != nil {
-		return nil, err
-	}
-	log.Println("formattedPath:", formattedPath)
-	req := &artifactregistrypb.GetDockerImageRequest{
-		Name: formattedPath,
-	}
-
-	resp, err := c.GetDockerImage(ctx, req)
-	if err != nil {
-		return nil, err
-	}
-
-	body, err := json.Marshal(resp)
-	if err != nil {
-		return nil, err
-	}
-	reader := strings.NewReader(string(body))
-
-	return reader, nil
-}
-
-func formatPath(path string) (string, error) {
+func newConfig() (*Config, error) {
 	project := os.Getenv("PROJECT")
 	if project == "" {
-		return "", fmt.Errorf("missing project")
+		return nil, fmt.Errorf("missing project")
 	}
 
 	repository := os.Getenv("REPOSITORY")
 	if repository == "" {
-		return "", fmt.Errorf("missing repository")
+		return nil, fmt.Errorf("missing repository")
 	}
 
 	region := os.Getenv("REGION")
@@ -161,19 +98,191 @@ func formatPath(path string) (string, error) {
 		region = "us-central1"
 	}
 
-	if strings.HasPrefix(path, "/") {
-		path = path[1:]
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = ":8080"
 	}
 
-	return fmt.Sprintf("projects/%s/locations/%s/repositories/%s/dockerImages/%s", project, region, repository, path), nil
+	credential := os.Getenv("GOOGLE_APPLICATION_CREDENTIALS")
+	if credential == "" {
+		return nil, fmt.Errorf("missing credential")
+	}
+
+	return &Config{
+		Project:    project,
+		Repository: repository,
+		Region:     region,
+		Port:       port,
+		Credential: credential,
+	}, nil
 }
+
+func initDB(ctx context.Context, config *Config, client *artifactregistry.Client) error {
+	formattedPath, err := formatPath(config)
+	if err != nil {
+		return err
+	}
+
+	req := &artifactregistrypb.ListDockerImagesRequest{
+		Parent: formattedPath,
+	}
+
+	it := client.ListDockerImages(ctx, req)
+	for {
+		resp, err := it.Next()
+		if err == iterator.Done {
+			break
+		}
+
+		if err != nil && err != iterator.Done {
+			return err
+		}
+
+		name, sha, err := extractNameAndSha(resp.Name)
+		if err != nil {
+			return err
+		}
+
+		var asset *Asset = &Asset{
+			Name:      name,
+			SHA:       sha,
+			RawName:   resp.Name,
+			URI:       resp.Uri,
+			MediaType: resp.MediaType,
+		}
+
+		for _, tag := range resp.Tags {
+			asset.Tags = append(asset.Tags, &tag)
+		}
+
+		RepositoryDB.Assets = append(RepositoryDB.Assets, asset)
+	}
+	return nil
+}
+
+func extractNameAndSha(input string) (name, sha string, err error) {
+	parts := strings.Split(input, "/")
+
+	if len(parts) < 2 {
+		return "", "", fmt.Errorf("invalid input format")
+	}
+
+	namePart := parts[len(parts)-1]
+	nameParts := strings.Split(namePart, "@")
+
+	if len(nameParts) != 2 {
+		return "", "", fmt.Errorf("invalid name and SHA format")
+	}
+
+	name = nameParts[0]
+	sha = nameParts[1]
+
+	return name, sha, nil
+}
+
+func getCredential(config *Config) (string, string, error) {
+	credentialFile, err := os.Open(config.Credential)
+	if err != nil {
+		return "", "", err
+	}
+	defer credentialFile.Close()
+
+	credentialBytes, err := io.ReadAll(credentialFile)
+	if err != nil {
+		return "", "", err
+	}
+
+	return "_json_key", string(credentialBytes), nil
+}
+
 func main() {
 	done := make(chan os.Signal, 1)
 	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
 
+	config, err := newConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	ctx := context.Background()
+	c, err := artifactregistry.NewClient(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer c.Close()
+
+	if err := initDB(ctx, config, c); err != nil {
+		log.Fatalf("failed to init db. error: %v", err)
+	}
+
+	client, err := registry.NewClient(registry.ClientOptDebug(true))
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	router := defaultRouter(nil)
-	router.Get("/{asset}", getAssetsHandler(ctx))
+	router.Get("/{assetName}@{assetSHA}", func(w http.ResponseWriter, r *http.Request) {
+		var assetName = chi.URLParam(r, "assetName")
+		var assetSHA = chi.URLParam(r, "assetSHA")
+		log.Println(assetName, assetSHA)
+		for _, asset := range RepositoryDB.Assets {
+			if asset.Name == assetName && asset.SHA == assetSHA {
+				user, credential, err := getCredential(config)
+				if err != nil {
+					log.Fatal(err)
+				}
+				err = client.Login(asset.URI, registry.LoginOptBasicAuth(
+					user,
+					credential,
+				))
+				if err != nil {
+					log.Fatal(err)
+				}
+				result, err := client.Pull(asset.URI)
+				if err != nil {
+					log.Fatal(err)
+				}
+				w.Header().Set("Content-Disposition", fmt.Sprintf("attachment; filename=%s-%s.tgz", result.Chart.Meta.Name, result.Chart.Meta.Version))
+				w.WriteHeader(http.StatusOK)
+				reader := bytes.NewReader(result.Chart.Data)
+				io.Copy(w, reader)
+				return
+			}
+		}
+	})
+
+	router.Get("/{assetName}:{assetTag}", func(w http.ResponseWriter, r *http.Request) {
+		var assetName = chi.URLParam(r, "assetName")
+		var assetTag = chi.URLParam(r, "assetTag")
+
+		for _, asset := range RepositoryDB.Assets {
+			if asset.Name == assetName {
+				for _, tag := range asset.Tags {
+					if *tag == assetTag {
+						user, credential, err := getCredential(config)
+						if err != nil {
+							log.Fatal(err)
+						}
+						err = client.Login(asset.URI, registry.LoginOptBasicAuth(
+							user,
+							credential,
+						))
+						if err != nil {
+							log.Fatal(err)
+						}
+						result, err := client.Pull(asset.URI)
+						if err != nil {
+							log.Fatal(err)
+						}
+						w.Header().Set("Content-Disposition", fmt.Sprintf("attachment; filename=%s-%s.tgz", result.Chart.Meta.Name, result.Chart.Meta.Version))
+						reader := bytes.NewReader(result.Chart.Data)
+						io.Copy(w, reader)
+						return
+					}
+				}
+			}
+		}
+	})
 
 	server := newServer(router)
 
